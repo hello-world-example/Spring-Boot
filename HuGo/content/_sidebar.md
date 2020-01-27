4bcb52863e9c6a08ba06c71e8d23ca72831cc38d
kailbin <ykb553@163.com>
2020-01-28 00:54:42 +0800

    7230_RFC-3986

--- HuGo/content/_sidebar.md
+++ HuGo/content/_sidebar.md
@@ -13,6 +13,7 @@ headless: true
   - [war 包部署]({{< relref "/docs/Action/War-Deploy.md" >}})
   - [热部署]({{< relref "/docs/Action/Hot-Swapping.md" >}})
   - [静态资源]({{< relref "/docs/Action/Static-Resources.md" >}})
+  - [RFC 7230 and RFC 3986 错误]({{< relref "/docs/Action/RFC-7230_RFC-3986.md" >}})
   - ~~[Testing]({{< relref "/docs/Unit-Test/index.md" >}})~~
   - ~~[Error Page]({{< relref "/docs/Error-Page.md" >}})~~
 
--- demo-spring-boot-lifecycle/src/main/java/xyz/kail/demo/spring/boot/lifecycle/LifecycleApplication.java
+++ demo-spring-boot-lifecycle/src/main/java/xyz/kail/demo/spring/boot/lifecycle/LifecycleApplication.java
@@ -1,15 +1,32 @@
 package xyz.kail.demo.spring.boot.lifecycle;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 需要继承 SpringBootServletInitializer
  */
 @SpringBootApplication
 public class LifecycleApplication extends SpringBootServletInitializer {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        });
+        return factory;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LifecycleApplication.class, args);
