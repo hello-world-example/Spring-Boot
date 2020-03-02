ef982fd8ee63d46cac9cfb4fe6b596d4a1768cca
kailbin <ykb553@163.com>
2020-03-02 18:20:39 +0800

    noting

--- demo-spring-boot-lifecycle/src/main/java/xyz/kail/demo/spring/boot/lifecycle/controller/IndexController.java
+++ demo-spring-boot-lifecycle/src/main/java/xyz/kail/demo/spring/boot/lifecycle/controller/IndexController.java
@@ -1,10 +1,18 @@
 package xyz.kail.demo.spring.boot.lifecycle.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 @Controller
 public class IndexController {
+
+    @Resource
+    private Environment environment;
 
     @GetMapping("/")
     public String index(boolean fail) {
@@ -13,6 +21,17 @@ public class IndexController {
         }
         System.out.println("index");
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/env")
+    public String env(String p) {
+        if (StringUtils.isEmpty(p)) {
+            return environment.toString();
+        }
+
+        "".hashCode();
+        return environment.getProperty(p);
     }
 
 }
